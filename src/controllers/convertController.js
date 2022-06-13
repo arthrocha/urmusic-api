f8341c2f425abc9449d9dccdb31c2a78f2ed45ae
Arthur <arthurderochaa@gmail.com>
2022-06-13 20:45:10 -0300

    limiting the number of characters in the parameters

--- src/controllers/convertController.js
+++ src/controllers/convertController.js
@@ -5,6 +5,9 @@ const path = require("path")
 
 module.exports = async function convertRequestController(req, res){
     const artist_or_music = req.params.artist_or_music
+    if(artist_or_music.length > 50){
+        return res.status(400).json({error:"titulo muito grande!"})
+    }
     const video_id = await youtubeURL(artist_or_music)
     await convertService(video_id, artist_or_music)
     setTimeout(async () => {
